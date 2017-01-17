5c92c700f956a250bf5f94526a284a917eabae3d
rahlk <i.m.ralk@gmail.com>
2017-01-17 13:52:45 -0500

    Lab Machine Changes

--- dataset/process.py
+++ dataset/process.py
@@ -7,25 +7,31 @@ from glob import glob
 from tabulate import tabulate
 
 
-def details(dir="./DataClass/"):
-    files = glob(os.path.join(os.path.abspath(dir), "*.csv"))
-    head = ["Name", "Samples", "% Smelly"]
-    rows = []
-    for file in files:
-        name = file.split("/")[-1].split(".csv")[0]
-        name = name.split("-")[0][:10]
-        dframe = pd.read_csv(file)
-        N = len(dframe)
-        p_smells = round(100 * sum([1 if s > 0 else 0 for s in dframe["SMELLS"].values]) / N, 0)
-        if N >= 8:
-            rows.append([name, N, p_smells])
-        else:
-            os.remove(file)
+def details():
+    for dir in ["DataClass", "GodClass", "LongMethod", "FeatureEnvy"]:
+        print(dir)
+        head = ["Dataset", "Samples", " Smelly (%)", "# metrics", "Nature"]
+        rows = []
+        files = glob(os.path.join(os.path.abspath(dir), "*.csv"))
+        nature = "Class" if dir == "DataClass" or dir == "GodClass" else "Method"
+        for file in files:
+            name = file.split("/")[-1].split(".csv")[0]
+            name = name.split("-")[0][:10]
+            dframe = pd.read_csv(file)
+            N = len(dframe)
+            n_metrics = len(dframe.columns)
+            smells = sum([1 if s > 0 else 0 for s in dframe["SMELLS"].values])
+            p_smells = round(100 * smells / N, 0)
+            if N >= 8:
+                rows.append([name, N, "{} ({})".format(smells, p_smells), n_metrics, nature])
+            else:
+                os.remove(file)
 
-    stats = pd.DataFrame(rows, columns=head)
-    stats.set_index("Name", inplace=False)
-    stats.sort_values("Samples", ascending=False, inplace=True)
-    print(tabulate(stats, headers=head, showindex="never", tablefmt="fancy_grid"))
+        stats = pd.DataFrame(rows, columns=head)
+        stats.set_index("Dataset", inplace=False)
+        stats.sort_values("Samples", ascending=False, inplace=True)
+        stats.to_csv(os.path.abspath(os.path.join(".", dir+".csv")))
+        print(tabulate(stats, headers=head, showindex="never"), end="\n\n")
     set_trace()
 
 
