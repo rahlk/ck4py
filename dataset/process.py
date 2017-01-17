ba2122bf994f33d88796159afbd7800fbf807cc5
rahlk <i.m.ralk@gmail.com>
2017-01-17 09:51:46 -0500

    MacBook Local Changes

--- dataset/process.py
+++ dataset/process.py
@@ -6,16 +6,18 @@ from pdb import set_trace
 from glob import glob
 from tabulate import tabulate
 
-def details(dir="./class/"):
+
+def details(dir="./DataClass/"):
     files = glob(os.path.join(os.path.abspath(dir), "*.csv"))
     head = ["Name", "Samples", "% Smelly"]
     rows = []
     for file in files:
         name = file.split("/")[-1].split(".csv")[0]
+        name = name.split("-")[0][:10]
         dframe = pd.read_csv(file)
         N = len(dframe)
         p_smells = round(100 * sum([1 if s > 0 else 0 for s in dframe["SMELLS"].values]) / N, 0)
-        if N>= 8:
+        if N >= 8:
             rows.append([name, N, p_smells])
         else:
             os.remove(file)
