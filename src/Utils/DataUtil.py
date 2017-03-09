7a543726b771386e7a283e521c7260f3b199d5f3
rahlk <i.m.ralk@gmail.com>
2017-03-09 18:01:59 -0500

    Lab Machine Changes

--- src/Utils/DataUtil.py
+++ src/Utils/DataUtil.py
@@ -23,7 +23,8 @@ def get_jar_paths():
             try:
                 path_dict[chunks[0]][chunks[1]].append(f)
             except Exception as e:
-                if e == chunks[0]:
+                print(str(e), chunks[0])
+                if e in chunks[0]:
                     path_dict.update({
                         chunks[0]: {
                             chunks[1]: [f]
