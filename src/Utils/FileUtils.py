e7b9dc4fa6cdbfae989caf45fe4278af8154d63b
rahlk <i.m.ralk@gmail.com>
2017-03-12 20:24:13 -0400

    Autocommit

--- src/Utils/FileUtils.py
+++ src/Utils/FileUtils.py
@@ -142,6 +142,7 @@ class XMLUtil:
 
     def get_metrics(self):
         metrics = self.metrics_as_list()
+        set_trace()
         return pd.DataFrame(metrics[1:], columns=metrics[0]).set_index("name")
 
     def save_as_csv(self):
